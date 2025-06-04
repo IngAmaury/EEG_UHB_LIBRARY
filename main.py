0463cb82c564760e5a6ef808e1dfdf7e166ff989
Amaury Santiago Horta <hortamaury@gmail.com>
2025-06-04 17:48:48 -0600

    Primer commit: estructura base y modularización

---
+++ main.py
@@ -0,0 +1,13 @@
+from eeg_uhb.acquisition import EEGAcquisitionManager
+
+if __name__ == "__main__":
+    EEG = EEGAcquisitionManager()
+    start_time = time.time()
+    duration = 15
+    try:
+        EEG.start_acquisition()
+        while time.time() - start_time < duration:
+            time.sleep(0.05)
+        EEG.stop_acquisition(save_path="./data", username="test_user")
+    except Exception as e:
+        print(f"Error: {e}")
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eeg_uhb",
+    version="0.1.0",
+    description="Librería modular para adquisición y procesamiento EEG de UHB",
+    author="Amaury Santiago Horta",
+    author_email="asantiagoh2300@alumno.ipn.mx",
+    url="https://github.com/IngAmaury/EEG_UHB_LIBRARY",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.26.4",
+        "pylsl==1.15.0",
+        "scipy==1.13.1",
+        "scikit-fuzzy==0.5.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
